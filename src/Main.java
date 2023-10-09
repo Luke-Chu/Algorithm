6fa3637074bf0d6a24aced2ece24d9250692e2d8
Luke <Luke-Chu@outlook.com>
2023-10-09 23:32:56 +0800

    First commit to init

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author ${USER}
+ * @Date ${YEAR}/${MONTH}/${DAY}
+ * @Time ${HOUR}:${MINUTE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
