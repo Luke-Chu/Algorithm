ba36054bbcfb83cfafc530986a12f79d5612a854
Luke-331 <Luke-Chu@outlook.com>
2024-01-06 14:02:12 +0800

    N皇后添加注释

--- Course/src/bt/NQueens.java
+++ Course/src/bt/NQueens.java
@@ -20,29 +20,30 @@ public class NQueens {
         for (char[] c : chessboard) {
             Arrays.fill(c, '-');
         }
-        backTrack(n, 0, chessboard);
+        backTrack(n, 0, chessboard);    //从0行开始
         for (var r : res) {
             System.out.println(r);
         }
     }
 
     private static void backTrack(int n, int row, char[][] chessboard) {
+        //n代表放了多少个皇后了，最多放n-1个，因为从0开始放的
         if (row == n) {
-            res.add(Array2List(chessboard));
+            res.add(Array2List(chessboard));    //放了n-1个了，是一种情况
             return;
         }
 
         for (int col = 0; col < n; ++col) {
             if (isValid(row, col, n, chessboard)) {
                 chessboard[row][col] = 'Q';
-                backTrack(n, row + 1, chessboard);  //递归
-                chessboard[row][col] = '-'; //回溯
+                backTrack(n, row + 1, chessboard);  //递归：当前行放了，直接下一行
+                chessboard[row][col] = '-'; //回溯：代表开始检查下一列
             }
         }
 
     }
 
-    public static List<String> Array2List(char[][] chessboard) {
+    private static List<String> Array2List(char[][] chessboard) {
         List<String> list = new ArrayList<>();
 
         for (char[] c : chessboard) {
@@ -53,19 +54,19 @@ public class NQueens {
 
     private static boolean isValid(int row, int col, int n, char[][] chessboard) {
         // 检查列
-        for (int i = 0; i < row; ++i) { // 相当于剪枝
+        for (int i = 0; i < row; ++i) { // col列从0行开始检查
             if (chessboard[i][col] == 'Q') {
                 return false;
             }
         }
         // 检查45度对角线
-        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {    //从左上角开始检查
             if (chessboard[i][j] == 'Q') {
                 return false;
             }
         }
         // 检查135度对角线
-        for (int i = row - 1, j = col + 1; i >= 0 && j <= n - 1; i--, j++) {
+        for (int i = row - 1, j = col + 1; i >= 0 && j <= n - 1; i--, j++) {    //从右上角开始检查
             if (chessboard[i][j] == 'Q') {
                 return false;
             }
